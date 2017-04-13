db017e4fa4d8060d778dfe0709fd4ff61b40c63f
freddy4212 <freddy4212@gmail.com>
2017-04-13 05:06:07 -0500

    dde-calendar_zh_CN.ts
    dde-calendar_zh_CN.ts

---
+++ translations/dde-calendar_zh_TW.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="zh_TW" version="2.1">
+<context>
+    <name>CalendarWindow</name>
+    <message>
+        <location filename="../calendarwindow.cpp" line="193"/>
+        <source>About</source>
+        <translation>關於</translation>
+    </message>
+    <message>
+        <location filename="../calendarwindow.cpp" line="32"/>
+        <location filename="../calendarwindow.cpp" line="198"/>
+        <source>Calendar</source>
+        <translation>Deepin 日曆</translation>
+    </message>
+    <message>
+        <location filename="../calendarwindow.cpp" line="194"/>
+        <source>Exit</source>
+        <translation>離開</translation>
+    </message>
+    <message>
+        <location filename="../calendarwindow.cpp" line="201"/>
+        <source>Deepin Calendar</source>
+        <translation>Deepin 日曆</translation>
+    </message>
+    <message>
+        <location filename="../calendarwindow.cpp" line="202"/>
+        <source>Version: %1</source>
+        <translation>版本：%1</translation>
+    </message>
+    <message>
+        <location filename="../calendarwindow.cpp" line="203"/>
+        <source>Calendar is a date tool.</source>
+        <translation>Deepin 日曆是個顯示日期的工具。</translation>
+    </message>
+</context>
+<context>
+    <name>InfoView</name>
+    <message>
+        <location filename="../infoview.cpp" line="25"/>
+        <source>Today</source>
+        <translation>今天</translation>
+    </message>
+</context>
+</TS>
